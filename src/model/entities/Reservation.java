e82a1c7c5a5b7721a77b784c76f9e34c144b931e
Afonso-cmd <afonso.ur@gmail.com>
2021-07-10 08:18:55 -0300

    good solution V1 exception java

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -50,19 +50,19 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates (Date checkIn, Date checkOut) {
+	public void updateDates (Date checkIn, Date checkOut) {
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Error in reservation: dates for update must be future dates";
+			//tratamento de exceção do java para dados inválidos
+			throw new IllegalArgumentException("Reservation dates for update must be future dates");
 		}
 		
 		if (!checkOut.after(checkIn)) {
-			return "Error in reservation: Check-out date must be after check-in date";
+			throw new IllegalArgumentException("Check-out date must be after check-in date");
 		}
 		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;//para indicar que não teve nenhum erro e fazer a atualização
 	}
 	
 	@Override
